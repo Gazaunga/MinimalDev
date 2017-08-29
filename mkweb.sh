ff879acb30e8c63d724e14b415ae92f6a5b0c5df
Gazaunga <Gazaunga@gmail.com>
2017-08-28 22:34:11 -0400

    Update mkweb.sh

--- mkweb.sh
+++ mkweb.sh
@@ -1,7 +1,7 @@
 # Create new web project folder and grab html5 boilerplate
 website() {
-mkdir $1
-cd $1
+mkdir $@
+cd @1
 git init
 wget https://raw.githubusercontent.com/Gazaunga/website/master/index.html
 git pull origin master
