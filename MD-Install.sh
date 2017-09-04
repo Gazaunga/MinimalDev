8dab585ab5f50a306f4c8eee5797ba8855b1d8ca
Gazaunga <Gazaunga@gmail.com>
2017-09-04 10:56:39 -0400

    Rename Notes.txt to MD-Install.sh

---
+++ MD-Install.sh
@@ -0,0 +1,53 @@
+packages=(
+  connman-gtk
+  termite
+  rofi-git
+  polybar-git
+  sddm
+  i3gaps-git
+  i3status-git
+  i3lock-fancy-git
+  nitrogen
+  compton
+  terminator
+  nerd-fonts-git
+  bumblebee-status-git
+  neovim
+  python-neovim
+  python2-neovim
+  ruby-neovim
+  emacs
+  otf-hermit
+  ttf-hack
+  typora
+  file-roller
+  zathura
+  zathura-pdf-mupdf
+  zathura-cb
+  zathura-ps
+  zathura-djvu
+  imagemagick
+  dunst
+  keepassx2
+  scrot
+  redshift
+  wpa_supplicant
+)
+pacaur -S --noconfirm --noedit ${packagelist[@]}
+
+# fancy
+cd ~
+curl https://raw.githubusercontent.com/scopatz/nanorc/master/install.sh | sh
+curl -sLf https://spacevim.org/install.sh | bash
+git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+wget https://raw.githubusercontent.com/Gazaunga/SpaceOS/master/.spacemacs
+
+# sddm
+cd ~
+sudo systemctl enable sddm.service
+git clone https://github.com/Eayu/sddm-theme-clairvoyance
+#sudo vim /etc/sddm.conf
+#set "Current" to "clairvoyance"
+
+cd $HOME/MinimalDev
+ruby mv.rb
